071c3f11247adcc286c9fd19f03ba6235c7cff3e
sptl-tech <patelsahil395@gmail.com>
2021-12-01 21:06:18 -0800

    Added additional front end code

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,14 @@ function App() {
       <h1>Health Management Application</h1>
 
       <div className = "form"> 
+        <label>Patient Name </label>
         <input type = "text" name = "patientName" />
+        <label>Patient ID </label>
         <input type = "text" name = "patientID" />
+        <label>Patient BMI </label>
         <input type = "text" name = "BMI" />
+
+        <button>Submit</button>
       </div>
       
     </div>
