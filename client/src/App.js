6fe4d587407c52ab6cdd737bce7451862651beee
sptl-tech <patelsahil395@gmail.com>
2021-12-01 19:41:29 -0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Health Management Application</h1>
+
+      <div className = "form"> 
+        <input type = "text" name = "patientName" />
+        <input type = "text" name = "patientID" />
+        <input type = "text" name = "BMI" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
