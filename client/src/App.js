b621bc869d15138fe7dde8e06f74b867d4ae5cdc
Edgar Villanueva <garvisteam@gmail.com>
2021-12-07 16:22:56 -0800

    Set up database

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
+import React from "react";
+import { useState } from "react";
+import Axios from 'axios'
 
 function App() {
+
+  const[name, setName] = useState("");
+  const[bmi, setBMI] = useState(0);
+
   return (
     <div className="App">
       <h1>Health Management Application</h1>
 
       <div className = "form"> 
         <label>Patient Name </label>
-        <input type = "text" name = "patientName" />
-        <label>Patient ID </label>
-        <input type = "text" name = "patientID" />
+        <input 
+          type = "text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        {/* <label>Patient ID </label>
+        <input type = "text" name = "patientID" /> */}
         <label>Patient BMI </label>
-        <input type = "text" name = "BMI" />
+        <input 
+          type = "number" 
+          onChange={(event) => {
+            setBMI(event.target.value);
+        }}
+        />
 
         <button>Submit</button>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
+const e = require("express");
 const express = require("express")
 const app = express()
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'healthSystem'
+});
+
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const bmi = req.body.age
+
+    db.query(
+        'INSERT INTO PATIENT (PatientName, PatientBMI) VALUES (?,?)',
+        (PatientName, PatientBMI),
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send("Values Inserted")
+            }
+        }
+    );
+})
 
 app.listen(3001, () => {
     console.log("Running on port 3001");
-})
+});
