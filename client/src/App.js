20073c877216fa8bc0fd27ab846c36afcd29993e
Edgar Villanueva <garvisteam@gmail.com>
2021-12-08 19:20:41 -0800

    Finished views

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,11 @@ import axios from 'axios'
 
  const App = () => {
 
-  const[PatientName, setName] = useState("");
-  const[PatientBMI, setBMI] = useState(0);
-  const[PatientAge, setAge] = useState(0);
-
-
+  const [PatientName, setName] = useState("");
+  const [PatientBMI, setBMI] = useState(0);
+  const [PatientAge, setAge] = useState(0);
   const [PatientList, setPatientList] = useState([]);
+  const [type, setType] = useState("all");
 
   const addPatient = () => {
     axios.post('http://localhost:3001/create', {
@@ -36,22 +35,58 @@ import axios from 'axios'
 
   const getHospitalView = () => {
     axios.get("http://localhost:3001/hosView").then((response) =>{
-      console.log(response);
+      setPatientList(response);
     })
   }
 
   const getDoctorView = () => {
     axios.get("http://localhost:3001/docView").then((response) =>{
-      console.log(response);
+      setPatientList(response);
     })
   }
 
   const getPatientView = () => {
     axios.get("http://localhost:3001/patView").then((response) =>{
-      console.log(response);
+      setPatientList(response);
     })
   }
 
+  const clickEvent = (type) => {
+    getPatients();
+    setType(type);
+  }
+
+  const insuranceProviders = ['Mobamba Care', 'Jahan Gold', 'Divided Health', 'GreenCross'];
+  const bloodType = ['O+', 'AB+', 'A-', 'B+', 'O-', 'B-'];
+
+  let view = (val) => {
+    switch (type) {
+      case 'hospital':
+        return(
+          <div className="patient">
+              <h3>Patient's Name: {val.PatientName}</h3>
+              <h3>Patient's Email: {val.PatientName.split(' ').join('.').toLowerCase() + '@gmail.com'}</h3>
+              <h3>Patient's Insurance: {insuranceProviders[val.PatientAge % (insuranceProviders.length)]}</h3>
+          </div>
+        );
+      case 'doctor':
+        return(
+          <div className="patient">
+              <h3>Patient's Name: {val.PatientName}</h3>
+              <h3>Patient's BMI: {val.PatientBMI}</h3>
+              <h3>Patient's Blood Type: {bloodType[val.PatientBMI % (bloodType.length)]}</h3>
+          </div>
+        );
+      default:
+        return(
+          <div className="patient">
+              <h3>Patient's Name: {val.PatientName}</h3>
+              <h3>Patient's BMI: {val.PatientBMI}</h3>
+              <h3>Patient's Age: {val.PatientAge}</h3>
+          </div>
+        );
+    }
+  }
 
   return (
     <div className="App">
@@ -65,8 +100,6 @@ import axios from 'axios'
             setName(event.target.value);
           }}
         />
-        {/* <label>Patient ID </label>
-        <input type = "text" name = "patientID" /> */}
         <label>Patient's BMI </label>
         <input 
           type = "number" 
@@ -85,14 +118,12 @@ import axios from 'axios'
         <button onClick={addPatient}>Submit</button>
       </div>
       <div className="patients">
-      <button onClick={getPatients}>Show Patients</button>
+      <button onClick={() => clickEvent('all')}>Show Patient View</button>
+      <button onClick={() => clickEvent('hospital')}>Show Hospital View</button>
+      <button onClick={() => clickEvent('doctor')}>Show Doctor View</button>
       {PatientList.map((val,key) => {
         return (
-          <div className="patient">
-             <h3>Patient's Name: {val.PatientName}</h3>
-             <h3>Patient's BMI: {val.PatientBMI}</h3>
-             <h3>Patient's Age: {val.PatientAge}</h3>
-          </div>
+          view(val)
         );
       })}
     </div>
